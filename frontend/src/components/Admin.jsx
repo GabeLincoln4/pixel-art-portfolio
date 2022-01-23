db66e1c32b6c2084f399cab5e4f04fb295154413
Gavin <7797864896@K12.LCS>
2022-01-22 21:04:10 -0500

    add event handler for input and textarea

--- frontend/src/components/Admin.jsx
+++ frontend/src/components/Admin.jsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function Admin(){
+
+    const [character, setCharacter] = useState({
+        name: '',
+        description: ''
+    });
+
+    function handleChange(e){
+        const {name, value} = e.target;
+
+        setCharacter(prevCharacters => {
+            return {
+                ...prevCharacters,
+                [name]: value
+            }
+        })
+    };
+
     return (<div>
         <h1>Admin Page</h1>
+        <form>
+            <h1>Name: </h1>
+            <input onChange={handleChange} type="text" name="name" value={character.name} />
+            <h1>Photo: </h1>
+            <input type="file" />
+            <h1>Description: </h1>
+            <textarea onChange={handleChange} name="description" type="text" rows="5" cols="25" value={character.description} />
+        </form>
     </div>);
 }
 
