1c5820a3b9f0ac804acac4d86423280b24470174
Gavin <7797864896@K12.LCS>
2022-02-16 16:09:37 -0500

    attempt to use use linked state for side bar animation logic

--- frontend/src/components/Test.jsx
+++ frontend/src/components/Test.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Exit from '../svg/Exit.jsx';
+import 'animate.css';
+import {useLinkedState} from "use-linked-state";
 
 
-function Test(){
+function Test(stateGateway){
+
+    const [sideMenu, setSideMenu] = useLinkedState(stateGateway);
+
+    const animatedEntrance = "animate__animated  animate__slideInRight absolute bg-blue-700 h-screen top-0 right-0 rounded-xl shadow-2xl w-1/2 z-20";
+    const animatedExit = "animate__animated  animate__slideOutRight absolute bg-blue-700 h-screen top-0 right-0 rounded-xl shadow-2xl w-1/2 z-20"
+
     return (
         
-        <div className="absolute bg-blue-700 h-screen top-0 right-0 rounded-xl shadow-2xl w-1/2 z-20">
+        <div className={sideMenu ? animatedEntrance : animatedExit }>
             <button>
                 <h1>Navbar</h1>
             </button>
